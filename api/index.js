f8d1eaf478b762de0c7c867ee2768ffd893b8db5
mohammad <122551005+moeJ8@users.noreply.github.com>
2025-02-03 22:11:14 +0300

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+});
+
 
 const app = express();
 const port = 3000;
