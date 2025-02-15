ff64f7bad7316fb82173cf1c27d9351d29f2f99f
mohammad <122551005+moeJ8@users.noreply.github.com>
2025-02-15 17:37:56 +0300

    private dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 
 export default function App() {
@@ -20,7 +21,9 @@ export default function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route element ={<PrivateRoute/>} >
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Route>
       <Route path="/projects" element={<Projects/>}/>
   
       </Routes>
