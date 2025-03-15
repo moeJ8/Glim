433d1d5c78a5ef481dcd5a1063a8bd40b41a4518
mohammad <122551005+moeJ8@users.noreply.github.com>
2025-03-16 01:49:17 +0300

    fix sign out error

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
     const {theme} = useSelector((state) => state.theme);
     const handleSignOut = async () => {
         try{
-          const res = await fetch('api/user/signout',{
+          const res = await fetch('/api/user/signout',{
             method: 'POST',
           });
           const data = await res.json();
