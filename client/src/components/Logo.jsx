50ba1a39df55cc3273c868d37f9901b5a3bdc1e9
mohammad <122551005+moeJ8@users.noreply.github.com>
2025-04-26 23:13:31 +0300

    logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 export default function Logo() {
   return (
     <Link to="/" className="self-center whitespace-nowrap">
-      <span className="bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 
-        dark:from-gray-400 dark:via-gray-200 dark:to-gray-400
-        bg-clip-text text-transparent
-        hover:from-gray-400 hover:via-white hover:to-gray-400
+      <span className="text-gray-600
+        dark:text-gray-200
+        hover:text-gray-700
+        dark:hover:text-white
         transition-all duration-300
         text-3xl sm:text-5xl md:text-4xl
         font-sans font-extrabold
         tracking-tight
-        drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)]">
+        drop-shadow-[0_1px_1px_rgba(0,0,0,0.3)]">
         Glim
       </span>
     </Link>
