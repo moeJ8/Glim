ebc930cb1ad8a5aa25cdb2c3e50845eb775ca082
mohammad <122551005+moeJ8@users.noreply.github.com>
2025-05-02 23:43:29 +0300

    Revert again

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,10 +6,7 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY, // ✅ Use import.meta.env
-  // Use auth subdomain for authentication
-  authDomain: window.location.hostname === "localhost" || window.location.hostname.includes("127.0.0.1")
-    ? "mern-blog-d9c14.firebaseapp.com"
-    : window.location.hostname === "glim.blog" ? "glim.blog" : "auth.glim.blog",
+  authDomain: "mern-blog-d9c14.firebaseapp.com",
   projectId: "mern-blog-d9c14",
   storageBucket: "mern-blog-d9c14.firebasestorage.app",
   messagingSenderId: "1075273492029",
